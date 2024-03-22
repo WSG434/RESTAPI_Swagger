79ff63c3d921cda1bc2506901fface4ccf44fa64
Vladimir Gordeev <WorkStationGuard@gmail.com>
2024-03-22 17:26:08 +0300

    Добавление JWT Auth, установка Laravel Breeze, настройка JWT Auth и работа с токеном в Swagger

--- app/Http/Controllers/Swagger/PostController.php
+++ app/Http/Controllers/Swagger/PostController.php
@@ -7,9 +7,40 @@ use App\Http\Controllers\Controller;
 /**
  *
  * @OA\POST(
+ *    path="/auth/login",
+ *    summary="Аутентификация",
+ *    tags={"User"},
+ *
+ *      @OA\RequestBody(
+ *          @OA\MediaType(
+ *              mediaType="application/json",
+ *                  @OA\Schema (
+ *                      @OA\Property(property="email", type="stirng", example="example@google.com"),
+ *                      @OA\Property(property="password", type="integer", example="12345678"),
+ *                  )
+ *          )
+ *      ),
+ *
+ *      @OA\Response(
+ *           response=200,
+ *           description="OK",
+ *           @OA\JsonContent(
+ *               @OA\Property(property="access_token", type="string", example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwODAvYXV0aC9sb2dpbiIsImlhdCI6MTcxMTExNTExNywiZXhwIjoxNzExMTE4NzE3LCJuYmYiOjE3MTExMTUxMTcsImp0aSI6Imhuc01RV2ZWVDJKYjRNNlMiLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.Ve8kh_Yl2bZKfkj05ofyHVu--8-CK_cKXd8fUsG2MCk"),
+ *               @OA\Property(property="token_type", type="stirng", example="bearer"),
+ *               @OA\Property(property="expires_in", type="integer", example="3600"),
+ *           )
+ *      )
+ *  ),
+ *
+ *
+ *
+ *
+ *
+ * @OA\POST(
  *   path="/api/posts",
  *   summary="Создание",
  *   tags={"Post"},
+ *   security={{ "bearerAuth": {} }},
  *
  *     @OA\RequestBody(
  *         @OA\MediaType(
@@ -40,6 +71,8 @@ use App\Http\Controllers\Controller;
  *    path="/api/posts",
  *    summary="Просмотр",
  *    tags={"Post"},
+ *    security={{ "bearerAuth": {} }},
+ *
  *
  *      @OA\Response(
  *           response=200,
@@ -61,6 +94,7 @@ use App\Http\Controllers\Controller;
  *     path="/api/posts/{post}",
  *     summary="Единичная запись",
  *     tags={"Post"},
+ *     security={{ "bearerAuth": {} }},
  *     @OA\Parameter(
  *         description="ID поста",
  *         in="path",
@@ -87,6 +121,7 @@ use App\Http\Controllers\Controller;
  *      path="/api/posts/{post}",
  *      summary="Обновление",
  *      tags={"Post"},
+ *      security={{ "bearerAuth": {} }},
  *      @OA\Parameter(
  *          description="ID поста",
  *          in="path",
@@ -123,6 +158,8 @@ use App\Http\Controllers\Controller;
  *      path="/api/posts/{post}",
  *      summary="Удаление",
  *      tags={"Post"},
+ *      security={{ "bearerAuth": {} }},
+ *
  *      @OA\Parameter(
  *          description="ID поста",
  *          in="path",
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,49 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Inertia\Inertia;
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('refresh', [\App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('me', [\App\Http\Controllers\AuthController::class, 'me']);
+});
+
+
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::except('/api/*');
+\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::except('/auth/*');
 
-Route::resource('/api/posts', \App\Http\Controllers\PostController::class);
+Route::resource('/api/posts', \App\Http\Controllers\PostController::class)->middleware('jwt.auth');
 
+
+
+require __DIR__ . '/auth.php';
